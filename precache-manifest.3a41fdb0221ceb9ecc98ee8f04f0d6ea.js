dbaa4cc4d85c33be2faebadaaaea9839d3173f82
Mridul Arya <arya7m.com>
2019-08-15 20:43:28 +0530

    Updates

---
+++ precache-manifest.3a41fdb0221ceb9ecc98ee8f04f0d6ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68001670e6217b8bf806a29c2599390f",
+    "url": "/monster_roladex/index.html"
+  },
+  {
+    "revision": "91eb6f5a3099db25441e",
+    "url": "/monster_roladex/static/css/main.6ef09735.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monster_roladex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "91eb6f5a3099db25441e",
+    "url": "/monster_roladex/static/js/main.b184a119.chunk.js"
+  },
+  {
+    "revision": "ba36a80f811b90862b68",
+    "url": "/monster_roladex/static/js/runtime~main.d6ed70fa.js"
+  }
+]);
